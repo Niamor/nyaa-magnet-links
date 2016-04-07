61373b9320bdfcd603c24d40f68636fd379a66b7
Niamor <niamor@users.noreply.github.com>
2016-04-07 04:31:10 +0200

    Only get magnet link if clicked on it

--- index.js
+++ index.js
@@ -1,38 +1,67 @@
+/*
+  Nyaa Magnet Links - a browser extension to add magnet links on NyaaTorrents.
+  Copyright (C) 2016 Romain B.
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  Home: https://github.com/Niamor/nyaa-magnet-links
+ */
+
 'use strict';
 
-const parseTorrent = require('parse-torrent');
-const url = document.URL;
+$(() => {
+  /* global $, parseTorrent, chrome */
+  const url = document.URL;
 
-if (url.indexOf('page=search') !== -1 ||
-  url.indexOf('user=') !== -1 ||
-  url.indexOf('page=') === -1) {
-  $(() => {
+  if (url.indexOf('page=search') !== -1 ||
+    url.indexOf('user=') !== -1 ||
+    url.indexOf('page=') === -1) {
     $('[title="Download"]').each((i, el) => {
       const elem = $(el);
-      parseTorrent.remote(location.protocol + elem.attr('href'), (err, parsedTorrent) => {
-        const magnetButton = $('<a/>', {
+      const magnetButton = $('<a/>', {
+        title: 'Magnet',
+        href: '#'
+      }).append($('<img/>', {
+        src: chrome.extension.getURL('icons/magnet-16.png'),
+        alt: 'Magnet'
+      })).click((e) => {
+        e.preventDefault();
+        parseTorrent.remote(window.location.protocol + elem.attr('href'),
+          (err, parsedTorrent) => {
+            if (err) console.log(err);
+            const magnetURI = parseTorrent.toMagnetURI(parsedTorrent);
+            magnetButton.attr('href', magnetURI);
+            window.location.href = magnetURI;
+          });
+      });
+      elem.before(magnetButton);
+    });
+  } else if (url.indexOf('page=view') !== -1) {
+    const button = $('.viewdownloadbutton');
+    parseTorrent.remote(window.location.protocol + button.find('a').attr('href'),
+      (err, parsedTorrent) => {
+        if (err) console.log(err);
+        const magnetButton = $('<div/>', {
+          class: 'viewdownloadbutton'
+        }).append($('<a/>', {
           title: 'Magnet',
           href: parseTorrent.toMagnetURI(parsedTorrent)
         }).append($('<img/>', {
-          src: chrome.extension.getURL('icons/magnet-16.png'),
+          src: chrome.extension.getURL('icons/magnet-26.png'),
           alt: 'Magnet'
-        }));
-        elem.before(magnetButton);
+        })));
+        button.after(magnetButton);
       });
-    });
-  });
-} else if (url.indexOf('page=view') !== -1) {
-  const button = $('.viewdownloadbutton');
-  parseTorrent.remote(location.protocol + button.find('a').attr('href'), (err, parsedTorrent) => {
-    const magnetButton = $('<div/>', {
-      class: 'viewdownloadbutton'
-    }).append($('<a/>', {
-      title: 'Magnet',
-      href: parseTorrent.toMagnetURI(parsedTorrent)
-    }).append($('<img/>', {
-      src: chrome.extension.getURL('icons/magnet-26.png'),
-      alt: 'Magnet'
-    })));
-    button.after(magnetButton);
-  });
-}
+  }
+});
